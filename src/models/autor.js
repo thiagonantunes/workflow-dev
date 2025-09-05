9924d12de9fd9a1037af82df811b6a8d350254b3
Thiago Antunes <thiagotna@hotmail.com>
2025-09-05 20:51:58 -0300

    feat: adicionado método para lista livros por autor

--- src/models/autor.js
+++ src/models/autor.js
@@ -3,66 +3,62 @@
 import db from '../db/dbconfig.js';
 
 class Autor {
-  constructor({
-    id,
-    nome,
-    nacionalidade,
-    created_at,
-    updated_at,
-  }) {
-    this.id = id || null;
-    this.nome = nome;
-    this.nacionalidade = nacionalidade;
-    this.created_at = created_at || new Date().toISOString();
-    this.updated_at = updated_at || new Date().toISOString();
-  }
+	constructor({ id, nome, nacionalidade, created_at, updated_at }) {
+		this.id = id || null;
+		this.nome = nome;
+		this.nacionalidade = nacionalidade;
+		this.created_at = created_at || new Date().toISOString();
+		this.updated_at = updated_at || new Date().toISOString();
+	}
 
-  static async pegarAutores() {
-    return db.select('*').from('autores');
-  }
+	static async pegarAutores() {
+		return db.select('*').from('autores');
+	}
 
-  static async pegarPeloId(id) {
-    const resultado = await db.select('*').from('autores').where({ id });
-    return resultado[0];
-  }
+	static async pegarPeloId(id) {
+		const resultado = await db.select('*').from('autores').where({ id });
+		return resultado[0];
+	}
 
-  async criar() {
-    const novoAutor = {
-      nome: this.nome,
-      nacionalidade: this.nacionalidade,
-      created_at: this.created_at,
-      updated_at: this.updated_at,
-    };
-    return db('autores').insert(novoAutor);
-  }
+	async criar() {
+		const novoAutor = {
+			nome: this.nome,
+			nacionalidade: this.nacionalidade,
+			created_at: this.created_at,
+			updated_at: this.updated_at,
+		};
+		return db('autores').insert(novoAutor);
+	}
 
-  async atualizar(id) {
-    // o update retorna a quantidade de rows atualizados e não o objeto do registro atualizado
-    await db('autores')
-      .where({ id })
-      .update({ ...this, updated_at: new Date().toISOString() });
+	async atualizar(id) {
+		// o update retorna a quantidade de rows atualizados e não o objeto do registro atualizado
+		await db('autores')
+			.where({ id })
+			.update({ ...this, updated_at: new Date().toISOString() });
 
-    return db.select('*').from('autores').where({ id });
-  }
+		return db.select('*').from('autores').where({ id });
+	}
 
-  static async excluir(id) {
-    // o del retorna a quantidade de rows deletados
-    return db('autores')
-      .where({ id })
-      .del();
-  }
+	static async excluir(id) {
+		// o del retorna a quantidade de rows deletados
+		return db('autores').where({ id }).del();
+	}
 
-  async salvar() {
-    // verificar se o id existe no banco
-    // se não existir é create
-    // se existir é update
-    if (this.id) {
-      const resultado = await this.atualizar(this.id);
-      return resultado;
-    }
-    const resultado = await this.criar();
-    return resultado;
-  }
+	async salvar() {
+		// verificar se o id existe no banco
+		// se não existir é create
+		// se existir é update
+		if (this.id) {
+			const resultado = await this.atualizar(this.id);
+			return resultado;
+		}
+		const resultado = await this.criar();
+		return resultado;
+	}
+
+	static async buscarLivrosPorAutor(autorId) {
+		return db('livros').where({ autor_id: autorId });
+	}
 }
 
 export default Autor;
